30ce3168006d13a4493d37f4712f582829f99893
Dave <david.mathaga@student.moringaschool.com>
2021-11-09 07:57:56 +0300

    make it better

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,29 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
-
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'eowqiepowqipeoeoqwpeoiwpqoei'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+  
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
+    
     from .models import User, Note
 
-    create_database(app)
-
-    login_manager = LoginManager()
+    login_manager =LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
@@ -31,8 +31,9 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    return app
+    create_database(app)
 
+    return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
