943da1eee4c02c9e58abfdd694954c5d976d9edd
Dave <david.mathaga@student.moringaschool.com>
2021-11-09 03:32:41 +0300

    database initialization

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,29 @@
-from re import A
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'eowqiepowqipeoeoqwpeoiwpqoei'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
-  
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
     from .models import User, Note
 
-    login_manager =LoginManager()
+    create_database(app)
+
+    login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
@@ -31,9 +31,8 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    create_database(app)
-
     return app
+
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,14 @@
-from flask import Blueprint,render_template,request,flash,redirect,url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from flask_login import login_user,login_required,logout_user,current_user
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET','POST'])
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -20,10 +22,11 @@ def login():
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
-
-
+        else:
+            flash('Email does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
+
 
 @auth.route('/logout')
 @login_required
@@ -32,7 +35,7 @@ def logout():
     return redirect(url_for('auth.login'))
 
 
-@auth.route('/sign-up', methods=['GET','POST'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -42,25 +45,22 @@ def sign_up():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists', category='error')
-
-        if len(email) < 4 :
-            flash('Email must be greater than 4 characters', category='error')
-        elif len(first_name) < 2 :
-            flash('First name must be greater than 1 characters', category='error')
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match', category='error')
-        elif len(password1) < 5:
-            flash('Passowrd must be atleast 5 characters', category='error')
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
-
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
-
             return redirect(url_for('views.home'))
 
-
-    return render_template('sign_up.html', user=current_user)
+    return render_template("sign_up.html", user=current_user)
