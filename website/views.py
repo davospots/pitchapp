d74228404df9670428b83e4177d8e2d0d47e4a31
Dave <david.mathaga@student.moringaschool.com>
2021-11-09 14:53:36 +0300

    on to making like database model

--- website/views.py
+++ website/views.py
@@ -82,3 +82,19 @@ def create_comment(post_id):
             flash('Pitch does not exist',category='error')
 
     return redirect(url_for('views.home'))
+
+@views.route('/delete-comment/<comment_id>')
+@login_required
+def def_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
+
